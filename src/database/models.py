e33106a7e954e9532609e6c35af63d2040edf966
Coulibaly Cheick Oumar <cheick@device-78.home>
2025-11-19 18:35:27 +0100

    feat(database): Implement PostgreSQL logging and fix context manager issue

---
+++ src/database/models.py
@@ -0,0 +1,25 @@
+#fichier pour la création des modèles de base de données
+
+from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+import json
+
+Base = declarative_base()
+
+class Interaction(Base):
+    """
+        Modèle qui enregistre la traçabilité des prédicions machine learning
+    """
+
+    __tablename__ = "ml_interactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+    #Outputs du modèle
+    prediction_class = Column(Integer, nullable=False)
+    probability_churn = Column(Float, nullable=False)
+
+    #Inputs pour l'analyse(stockage des entrées complètes pour l'analyse future)
+    raw_input_json = Column(String)
